6177006f18efa2903ebcc8b132330b4336fc3bf6
Charles Del Rosario <charlesadriel.delrosario@gmail.com>
2025-01-20 16:51:25 +0800

    hmm

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
 import styled from 'styled-components'
 import Home from './pages/Home'
 import Agents from './pages/Agents'
